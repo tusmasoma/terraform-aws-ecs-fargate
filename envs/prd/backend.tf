de57a859a76cedcf3a4193063242624fcf03189c
tusmasoma <s.k18@icloud.com>
2025-01-16 18:13:32 +0900

    chore: tfstate file management on remote

--- envs/prd/backend.tf
+++ envs/prd/backend.tf
@@ -1,8 +1,7 @@
-// TODO: 一旦、ローカルのterraform.tfstateを使用するためコメントアウト
-# terraform {
-#   backend "s3" {
-#     bucket         = "tf-s3-state-lock-example-tfstate"
-#     key            = "terraform.tfstate"
-#     dynamodb_table = "example-state-lock-table"
-#   }
-# }
+terraform {
+  backend "s3" {
+    bucket         = "tf-s3-state-lock-example-tfstate"
+    key            = "terraform.tfstate"
+    use_lockfiles  = true
+  }
+}
